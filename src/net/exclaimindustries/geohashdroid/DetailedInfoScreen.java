e03e81c92a3daa365d81e21f30a9ddf3f780e323
Nicholas Killewald <captainspam@gmail.com>
2009-04-06 06:02:38 +0000

    Created the DetailedInfoScreen class.  It doesn't do anything yet, but it's at least there for now.  All it really needs is a design and some data-passing logic.
    git-svn-id: https://geohashdroid.googlecode.com/svn/trunk@44 7ebfa8b6-0479-11de-af5c-836599625e1f

---
+++ src/net/exclaimindustries/geohashdroid/DetailedInfoScreen.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C)2009 Nicholas Killewald
+ * 
+ * This file is distributed under the terms of the BSD license.
+ * The source package should have a LICENCE file at the toplevel.
+ */
+package net.exclaimindustries.geohashdroid;
+
+import android.app.Activity;
+
+/**
+ * The <code>DetailedInfoScreen</code> displays, in detail, just where the user
+ * is right now.  With big ol' text and lots of decimal places.  This is ideal
+ * for, say, taking pictures of the phone when at the point, at least until the
+ * picture-taking-and-tagging function is coded up.
+ * 
+ * @author Nicholas Killewald
+ */
+public class DetailedInfoScreen extends Activity {
+
+}
