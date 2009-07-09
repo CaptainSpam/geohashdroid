4de10bd65d1d6aa08b0c6bcadc034fc45a648da2
Nicholas Killewald <captainspam@gmail.com>
2009-07-09 01:15:02 +0000

    Expanded imports in HashMaker.
    git-svn-id: https://geohashdroid.googlecode.com/svn/trunk@131 7ebfa8b6-0479-11de-af5c-836599625e1f

--- src/net/exclaimindustries/geohashdroid/HashMaker.java
+++ src/net/exclaimindustries/geohashdroid/HashMaker.java
@@ -7,9 +7,14 @@
  */
 package net.exclaimindustries.geohashdroid;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.net.HttpURLConnection;
 import java.util.Calendar;
-import java.net.*;
-import java.io.*;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
