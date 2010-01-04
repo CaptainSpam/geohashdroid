05ab35ababfd74544e9c1cf0531a71c59ff73ca7
Nicholas Killewald <captainspam@gmail.com>
2010-01-04 05:47:27 +0000

    Added in a workaround to make the location overlay work on the Motorola CLIQ (and presumably, anything else that doesn't implement the MyLocationOverlay drawables properly).
    git-svn-id: https://geohashdroid.googlecode.com/svn/trunk@296 7ebfa8b6-0479-11de-af5c-836599625e1f

--- src/net/exclaimindustries/geohashdroid/AutoZoomingLocationOverlay.java
+++ src/net/exclaimindustries/geohashdroid/AutoZoomingLocationOverlay.java
@@ -8,6 +8,11 @@
 package net.exclaimindustries.geohashdroid;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Paint.Style;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,6 +21,7 @@ import android.os.Message;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.Projection;
 
 /**
  * <p>
@@ -47,6 +53,20 @@ public class AutoZoomingLocationOverlay extends MyLocationOverlay {
     // problems are related to living in the state of Kentucky. :-)
     private boolean mHaveGPSFix = false;
 
+    // Gets set to true if an error occurs when drawing the point the normal
+    // way.  This means we're using a Motorola CLIQ or any other device that
+    // doesn't properly implement the MyLocationListener drawable.
+    private boolean mBugged = false;
+    
+    // We want to hold on to all of these so we don't need to keep re-obtaining
+    // them over and over and over again.
+    private Drawable mIndicator;
+    private int mIndicatorWidth;
+    private int mIndicatorHeight;
+    private Point mIndicatorLeft;
+    private Point mIndicatorCenter;
+    private Paint mIndicatorAccuracyPaint;
+    
     /**
      * Message indicating the location was changed and the object sent back is,
      * in fact, a GeoPoint.
@@ -169,4 +189,71 @@ public class AutoZoomingLocationOverlay extends MyLocationOverlay {
         mFirstFix = true;
         return super.runOnFirstFix(runnable);
     }
+
+    @Override
+    protected void drawMyLocation(Canvas canvas, MapView mapView, Location lastFix, GeoPoint myLocation, long when) {
+        // This needs to be overridden to account for devices like the Motorola
+        // CLIQ which can't deal with the MyLocationOverlay object.  This was
+        // all found in the Motorola developer forums.
+        if(!mBugged) {
+            // Try to draw this the normal way first.  If we fail, we'll know
+            // this went wrong.
+            try {
+                super.drawMyLocation(canvas, mapView, lastFix, myLocation, when);
+            } catch(Exception e) {
+                mBugged = true;
+            }
+        }
+
+        // So, if we're bugged at this point, let's do it the hard way...
+        if(mBugged) {
+            if (mIndicator == null) {
+                // If this is the first time through, get all our paints and
+                // values and such so we don't need to do this repeatedly.
+                mIndicatorAccuracyPaint = new Paint();
+                mIndicatorAccuracyPaint.setAntiAlias(true);
+                mIndicatorAccuracyPaint.setStrokeWidth(2.0f);
+               
+                mIndicator = mapView.getContext().getResources().getDrawable(R.drawable.ic_maps_indicator_current_position);
+                mIndicatorWidth = mIndicator.getIntrinsicWidth();
+                mIndicatorWidth = mIndicator.getIntrinsicHeight();
+                mIndicatorCenter = new Point();
+                mIndicatorLeft = new Point();
+            }
+            
+            // Now, commence drawing!
+            Projection projection = mapView.getProjection();
+            
+            // First, we need the location and accuracy of the fix.
+            double latitude = lastFix.getLatitude();
+            double longitude = lastFix.getLongitude();
+            float accuracy = lastFix.getAccuracy();
+           
+            // We also need to calculate out how far degrees are in this
+            // particular projection for the accuracy-o-meter thingy.  This
+            // should be close enough.
+            float[] result = new float[1];
+
+            Location.distanceBetween(latitude, longitude, latitude, longitude + 1, result);
+            float longitudeLineDistance = result[0];
+            
+            GeoPoint leftGeo = new GeoPoint((int)(latitude*1e6), (int)((longitude-accuracy/longitudeLineDistance)*1e6));
+            projection.toPixels(leftGeo, mIndicatorLeft);
+            projection.toPixels(myLocation, mIndicatorCenter);
+            int radius = mIndicatorCenter.x - mIndicatorLeft.x;
+
+            // And now, paint!
+            mIndicatorAccuracyPaint.setColor(0xff6666ff);
+            mIndicatorAccuracyPaint.setStyle(Style.STROKE);
+            canvas.drawCircle(mIndicatorCenter.x, mIndicatorCenter.y, radius, mIndicatorAccuracyPaint);
+    
+            mIndicatorAccuracyPaint.setColor(0x186666ff);
+            mIndicatorAccuracyPaint.setStyle(Style.FILL);
+            canvas.drawCircle(mIndicatorCenter.x, mIndicatorCenter.y, radius, mIndicatorAccuracyPaint);
+                       
+            mIndicator.setBounds(mIndicatorCenter.x - mIndicatorWidth / 2, mIndicatorCenter.y - mIndicatorHeight / 2, mIndicatorCenter.x + mIndicatorWidth / 2, mIndicatorCenter.y + mIndicatorHeight / 2);
+            mIndicator.draw(canvas);
+        }
+    }
+    
 }
