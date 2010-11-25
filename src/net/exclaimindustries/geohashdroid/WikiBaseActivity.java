2c438942749e2c898763575d87463fc484684654
Nicholas Killewald <captainspam@gmail.com>
2010-11-25 20:49:42 +0000

    Removed unused import.
    WikiBaseActivity is a LocationAwareActivity, so it sort of doesn't need the
    Location import anymore.
    
    git-svn-id: https://geohashdroid.googlecode.com/svn/trunk@536 7ebfa8b6-0479-11de-af5c-836599625e1f

--- src/net/exclaimindustries/geohashdroid/WikiBaseActivity.java
+++ src/net/exclaimindustries/geohashdroid/WikiBaseActivity.java
@@ -19,7 +19,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.DialogInterface.OnCancelListener;
-import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
