994bba413ef4f03e133a42fcf7585b058f0b8b51
Nicholas Killewald <captainspam@gmail.com>
2012-05-23 16:07:06 -0400

    Added another Intent-checker in AndroidUtil.
    This new convenience method will check a complete Intent, not just an
    action string, for whether or not there'll be an Activity waiting for
    it.

--- src/net/exclaimindustries/tools/AndroidUtil.java
+++ src/net/exclaimindustries/tools/AndroidUtil.java
@@ -27,11 +27,24 @@ public class AndroidUtil {
      *         responded to, false otherwise.
      */
     public static boolean isIntentAvailable(Context context, String action) {
-        final PackageManager packageManager = context.getPackageManager();
         final Intent intent = new Intent(action);
+        return isIntentAvailable(context, intent);
+    }
+
+    /**
+     * Indicates whether the specified Intent has any Activity that will respond
+     * to it.  This comes into play if you need to check against an Intent with
+     * a more specific specifier than just the action (i.e. an ACTION_VIEW
+     * Intent, where the given URI determines what starts it).
+     *
+     * @param context the application's environment
+     * @param intent the Intent to check against
+     * @return true if an Activity would respond to that Intent, false otherwise
+     */
+    public static boolean isIntentAvailable(Context context, Intent intent) {
+        final PackageManager packageManager = context.getPackageManager();
         List<ResolveInfo> list = packageManager.queryIntentActivities(intent,
                 PackageManager.MATCH_DEFAULT_ONLY);
         return list.size() > 0;
     }
-
 }
