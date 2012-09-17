ebba6affcbe372a87aa6b7b67c4a4371b9761981
Nicholas Killewald <captainspam@gmail.com>
2012-09-17 00:05:14 -0400

    Fleshed out more of StockService.
    This isn't at all done yet, of course, but more of the skeleton is in
    place for later.  Don't actually run this yet, since StockService's
    WakeLock won't ever be released.

--- src/net/exclaimindustries/geohashdroid/StockService.java
+++ src/net/exclaimindustries/geohashdroid/StockService.java
@@ -7,6 +7,7 @@
  */
 package net.exclaimindustries.geohashdroid;
 
+import java.lang.ref.WeakReference;
 import java.util.Calendar;
 import java.util.TimeZone;
 
@@ -16,7 +17,10 @@ import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.PowerManager;
 //import android.util.Log;
 
 /**
@@ -38,15 +42,31 @@ import android.os.IBinder;
  */
 public class StockService extends Service {
     
-//    private static final String DEBUG_TAG = "StockService";
+    private static final String DEBUG_TAG = "StockService";
+    
+    private static PowerManager.WakeLock mWakeLock;
+    
+    private static final Graticule DUMMY_YESTERDAY = new Graticule(51, false, 0, true);
+    private static final Graticule DUMMY_TODAY = new Graticule(38, false, 84, true);
+    
+    private HashBuilder.StockRunner mRunner;
     
     public static class StockBroadcastReceiver extends BroadcastReceiver {
 
         @Override
-        public void onReceive(Context c, Intent i) {
+        public void onReceive(Context c, Intent i)  {
+            // Good morning!  Now STAY awake until we're done here.  If the
+            // wakelock is already acquired, don't bother; it'll be freed 
+            mWakeLock = ((PowerManager)c
+                    .getSystemService(Context.POWER_SERVICE)).newWakeLock(
+                    PowerManager.PARTIAL_WAKE_LOCK, DEBUG_TAG);
+            
+            mWakeLock.acquire();
+            
             // If this is the boot broadcast, start the service.
             if(i.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
                 Intent serviceIntent = new Intent(c, StockService.class);
+                serviceIntent.setAction(i.getAction());
                 c.startService(serviceIntent);
             }
         }
@@ -62,6 +82,81 @@ public class StockService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+    
+    /**
+     * <p>
+     * This is the basic Handler for the StockRunner.  This assumes the usual
+     * StockService <i>automated</i> operation; that is, it's expecting to be in
+     * a process of checking TWO stocks.  If the response it gets says it was
+     * checking a 30W stock, it'll fire off another check for the non-30W stock.
+     * If the response is non-30W, it'll consider itself done and report any
+     * errors it ran into along the way.
+     * </p>
+     * 
+     * <p>
+     * All messages to this should be handled in a timely manner.  Handler
+     * leaking should not be an issue.
+     * </p>
+     * 
+     * @author Nicholas Killewald
+     */
+    private static class ResponseHandler extends Handler {
+        private WeakReference<StockService> mService;
+        
+        public ResponseHandler(StockService service) {
+            mService = new WeakReference<StockService>(service);
+        }
+        
+        public void handleMessage(Message message) {
+            // Response!
+            Info info = (Info)message.obj;
+            
+            // First, what happened?
+            if(message.what == HashBuilder.StockRunner.ABORTED) {
+                // If we aborted, then just clear the notification and forget
+                // about it.
+                
+                // TODO: Do so!
+            } else if(message.what == HashBuilder.StockRunner.ERROR_NOT_POSTED) {
+                // If it wasn't posted yet, we need to send up a notification
+                // AND schedule another check later.
+                
+                // TODO: Do so!
+            } else if(message.what == HashBuilder.StockRunner.ERROR_SERVER) {
+                // A server error can mean any of a wide variety of things.  For
+                // our purposes, we should give up; if it's something transient
+                // (i.e. bailed because the network connection vanished), the
+                // main service will reschedule it when the network returns.
+                
+                // TODO: Do so, as well as the NetworkManager callbacks!
+            } else {
+                // Otherwise, we should be good to go!
+                
+                // TODO: Handle the next day, if need be!  We can even recycle
+                // the Handler object!
+            }
+        }
+    }
+    
+    private void doStockFetching(boolean yesterday) {
+        // Remember, this DOES NOT CARE if the stock is already cached.  Do that
+        // check FIRST!  It's a static call on HashBuilder!
+        
+        // First, kill the previous StockRunner, if it was in progress somehow.
+        if(mRunner != null && mRunner.getStatus() == HashBuilder.StockRunner.BUSY) {
+            mRunner.abort();
+        }
+        
+        mRunner = HashBuilder.requestStockRunner(this, Calendar.getInstance(),
+                (yesterday ? DUMMY_YESTERDAY : DUMMY_TODAY),
+                new ResponseHandler(this));
+        
+        // We don't need to keep track of the thread.  StockRunner should do
+        // that for us.
+        Thread thread = new Thread(mRunner);
+        thread.setName("StockServiceRunnerThread");
+        thread.start();
+    }
 
     @Override
     public void onCreate() {
@@ -72,7 +167,6 @@ public class StockService extends Service {
         // as soon as we begin without waiting a day until the alarm goes off
         // for the first time.
         
-        // TODO: Do just that!
         
         // Second, set the alarm.  We're aiming at 9:30am ET (2:30pm UTC).  The
         // NYSE opens at 9:00am ET, but in the interests of possible clock
@@ -100,6 +194,11 @@ public class StockService extends Service {
                 alarmTime.getTimeInMillis(),
                 AlarmManager.INTERVAL_DAY,
                 PendingIntent.getBroadcast(this, 0, alarmIntent, 0));
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
     }
 
 }
