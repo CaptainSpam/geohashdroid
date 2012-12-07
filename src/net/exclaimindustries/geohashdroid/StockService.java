11021af62cc82960eb566e018b90ef3bcb8ba9a4
Nicholas Killewald <captainspam@gmail.com>
2012-12-07 01:14:57 -0500

    More fleshing out of StockService.
    Ha!  Thought I was dead, did ya?
    
    StockService is now fleshed out with a lot more of what'll make it what it
    is.  It'll know to attempt to reconnect if the network goes down while
    refreshing, it'll get both stock prices for the day (30W and non-30W) if
    need be, and it's just all coming together slowly.

--- src/net/exclaimindustries/geohashdroid/StockService.java
+++ src/net/exclaimindustries/geohashdroid/StockService.java
@@ -11,12 +11,16 @@ import java.lang.ref.WeakReference;
 import java.util.Calendar;
 import java.util.TimeZone;
 
+import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -49,19 +53,43 @@ public class StockService extends Service {
     private static final Graticule DUMMY_YESTERDAY = new Graticule(51, false, 0, true);
     private static final Graticule DUMMY_TODAY = new Graticule(38, false, 84, true);
     
-    private HashBuilder.StockRunner mRunner;
+    private NetworkReceiver mNetReceiver = new NetworkReceiver();
     
+    private HashBuilder.StockRunner mRunner;
+
+    private AlarmManager mAlarmManager;
+    
+    /**
+     * This handles all wakelockery.
+     * 
+     * This may be overly complicated.  I'm open to suggestions to change it.
+     * 
+     * @param c Context from which a wakelock will be gotten
+     * @param acquire true to acquire a lock, false to release it
+     */
+    @SuppressLint("Wakelock")
+    private static void doWakeLockery(Context c, boolean acquire) {
+        // First, get a wakelock if we need one.
+        if(mWakeLock == null)
+            mWakeLock = ((PowerManager)c
+                    .getSystemService(Context.POWER_SERVICE)).newWakeLock(
+                    PowerManager.PARTIAL_WAKE_LOCK, DEBUG_TAG);
+        
+        if(acquire) {
+            if(!mWakeLock.isHeld()) mWakeLock.acquire();
+        } else {
+            if(mWakeLock.isHeld()) mWakeLock.release();
+        }
+    }
+
     public static class StockBroadcastReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context c, Intent i)  {
             // Good morning!  Now STAY awake until we're done here.  If the
-            // wakelock is already acquired, don't bother; it'll be freed 
-            mWakeLock = ((PowerManager)c
-                    .getSystemService(Context.POWER_SERVICE)).newWakeLock(
-                    PowerManager.PARTIAL_WAKE_LOCK, DEBUG_TAG);
-            
-            mWakeLock.acquire();
+            // wakelock is already acquired, don't bother; it'll be freed when
+            // we're done.
+            doWakeLockery(c, true);
             
             // If this is the boot broadcast, start the service.
             if(i.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
@@ -72,8 +100,31 @@ public class StockService extends Service {
         }
         
     }
+    
+    /**
+     * This receiver isn't declared in the manifest because we want to be able
+     * to shut it off if we don't want it on.  In general, it should ONLY go on
+     * if we run into a network issue and are waiting for the connection to come
+     * back up, at which time we can try firing off a stock check again.
+     */
+    private class NetworkReceiver extends BroadcastReceiver {
 
-    private AlarmManager mAlarmManager;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(isConnected()) {
+                doWakeLockery(context, true);
+                
+                // NETWORK'D!!! Unregister ourselves from broadcasts and fire
+                // off those checks!
+                context.unregisterReceiver(this);
+                if(!doAllStockDbChecks()) {
+                    // If no stock checks needed to be done, release the
+                    // wakelock right away.
+                    doWakeLockery(context, false);
+                }
+            }
+        }
+    }
     
     /* (non-Javadoc)
      * @see android.app.Service#onBind(android.content.Intent)
@@ -97,8 +148,6 @@ public class StockService extends Service {
      * All messages to this should be handled in a timely manner.  Handler
      * leaking should not be an issue.
      * </p>
-     * 
-     * @author Nicholas Killewald
      */
     private static class ResponseHandler extends Handler {
         private WeakReference<StockService> mService;
@@ -110,6 +159,7 @@ public class StockService extends Service {
         public void handleMessage(Message message) {
             // Response!
             Info info = (Info)message.obj;
+            StockService service = mService.get();
             
             // First, what happened?
             if(message.what == HashBuilder.StockRunner.ABORTED) {
@@ -117,23 +167,47 @@ public class StockService extends Service {
                 // about it.
                 
                 // TODO: Do so!
+                doWakeLockery(service, false);
             } else if(message.what == HashBuilder.StockRunner.ERROR_NOT_POSTED) {
                 // If it wasn't posted yet, we need to send up a notification
                 // AND schedule another check later.
                 
                 // TODO: Do so!
+                doWakeLockery(service, false);
             } else if(message.what == HashBuilder.StockRunner.ERROR_SERVER) {
-                // A server error can mean any of a wide variety of things.  For
-                // our purposes, we should give up; if it's something transient
-                // (i.e. bailed because the network connection vanished), the
-                // main service will reschedule it when the network returns.
+                // A server error can mean any of a wide variety of things.  If
+                // we're not connected right now, we can pretty reliably assume
+                // said thing is in the variety of "it failed because there's no
+                // network connection".
+                if(!service.isConnected()) {
+                    // So if that's the case, the NetworkReceiver can kick in.
+                    IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+                    service.registerReceiver(service.mNetReceiver, filter);
+                } else {
+                    // If that's NOT the case, give up.  The remaining wide
+                    // variety of things is not worth thinking about.
+                    
+                    // TODO: NOTIFICATION!!!
+                }
                 
-                // TODO: Do so, as well as the NetworkManager callbacks!
+                // In any event, release the wakelock.  We'll be awake for
+                // whatever happens to need us later.
+                doWakeLockery(service, false);
             } else {
-                // Otherwise, we should be good to go!
-                
-                // TODO: Handle the next day, if need be!  We can even recycle
-                // the Handler object!
+                // Otherwise, we should be good to go!  If this was 30W, go get
+                // the non-30W data.  If this was non-30W, stop; we're all done
+                // here.
+                if(!info.uses30WRule() && !HashBuilder.hasStockStored(service, info.getCalendar(), DUMMY_TODAY)) {
+                    service.doStockFetching(false);
+                    
+                    // Oh, and don't release the wakelock juuuuuust yet.  Still
+                    // need to finish fetching.
+                }
+                else
+                {
+                    // Okay, NOW release the wakelock.
+                    doWakeLockery(service, false);
+                }
             }
         }
     }
@@ -141,6 +215,8 @@ public class StockService extends Service {
     private void doStockFetching(boolean yesterday) {
         // Remember, this DOES NOT CARE if the stock is already cached.  Do that
         // check FIRST!  It's a static call on HashBuilder!
+        
+        // TODO: NOTIFICATION!!!
         
         // First, kill the previous StockRunner, if it was in progress somehow.
         if(mRunner != null && mRunner.getStatus() == HashBuilder.StockRunner.BUSY) {
@@ -165,8 +241,17 @@ public class StockService extends Service {
         // First off, get today's stocks, if need be and if possible, as soon as
         // we start the service.  That'll ensure we're in a fully-cached state
         // as soon as we begin without waiting a day until the alarm goes off
-        // for the first time.
+        // for the first time.  This'll also kick off a new thread, so we can
+        // move forward past this.
+        //
+        // TODO: Should I check to make sure we're not trying to get a "today"
+        // too early?  That is, if it's before 9:30am "today", there won't be
+        // a stock yet, and that'll return an error...
+        boolean shouldReleaseWakelock = false;
         
+        if(isConnected()) {
+            shouldReleaseWakelock = doAllStockDbChecks();
+        }
         
         // Second, set the alarm.  We're aiming at 9:30am ET (2:30pm UTC).  The
         // NYSE opens at 9:00am ET, but in the interests of possible clock
@@ -194,6 +279,8 @@ public class StockService extends Service {
                 alarmTime.getTimeInMillis(),
                 AlarmManager.INTERVAL_DAY,
                 PendingIntent.getBroadcast(this, 0, alarmIntent, 0));
+        
+        if(shouldReleaseWakelock) doWakeLockery(this, false);
     }
 
     @Override
@@ -201,4 +288,38 @@ public class StockService extends Service {
         super.onStart(intent, startId);
     }
 
+    private boolean isConnected() {
+        // This just checks if we've got any valid network connection at all.
+        ConnectivityManager connMgr = (ConnectivityManager) 
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+    
+    private boolean doAllStockDbChecks() {
+        // This does the database checks and fires off doStockFetching if need
+        // be.  It returns true if we need to go to the network, false if we
+        // have everything we need.
+        
+        // Why yes, I AM accounting for the vanishingly slim possibility
+        // that the boundaries of a day might pass between the two calls
+        // of hasStockStored below.  Thank you for noticing.
+        Calendar cal = Calendar.getInstance();
+        
+        // Go to the database!  This should be a relatively painless call.
+        if(!HashBuilder.hasStockStored(this, cal, DUMMY_YESTERDAY)) {
+            // No stock for yesterday.  Fetch!  Note that this will also
+            // grab today's if need be after it's done.
+            doStockFetching(true);
+            return true;
+        } else if(!HashBuilder.hasStockStored(this, cal, DUMMY_TODAY)) {
+            // No stock for today.  Fetch!
+            doStockFetching(false);
+            return true;
+        }
+        
+        // If we fell out of the if statements, we have all the stocks we
+        // need.  Return false to let the caller know.
+        return false;
+    }
 }
