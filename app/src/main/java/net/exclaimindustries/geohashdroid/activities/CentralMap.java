f5be666eb0179106b20467e537dac49d40c4a8cb
Nicholas Killewald <captainspam@gmail.com>
2015-04-10 16:42:11 -0400

    Created ErrorBanner.
    This handy banner will slide in and out of the top of CentralMap any time
    there's some error to deal with.  Styling is still up for debate.

--- app/src/main/java/net/exclaimindustries/geohashdroid/activities/CentralMap.java
+++ app/src/main/java/net/exclaimindustries/geohashdroid/activities/CentralMap.java
@@ -8,13 +8,19 @@
 package net.exclaimindustries.geohashdroid.activities;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import net.exclaimindustries.geohashdroid.R;
+import net.exclaimindustries.geohashdroid.services.StockService;
+import net.exclaimindustries.geohashdroid.util.Info;
+import net.exclaimindustries.geohashdroid.widgets.ErrorBanner;
 
 /**
  * CentralMap replaces MainMap as the map display.  Unlike MainMap, it also
@@ -24,12 +30,65 @@ import net.exclaimindustries.geohashdroid.R;
  */
 public class CentralMap extends Activity {
     private boolean mSelectAGraticule = false;
+    private Info mCurrentInfo;
+
+    private ErrorBanner mBanner;
+
+    private BroadcastReceiver mStockReceiver = new BroadcastReceiver() {
+        private int mWaitingOnThisOne = -1;
+
+        public void setWaitingId(int id) {
+            mWaitingOnThisOne = id;
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // A stock result arrives!  Let's make sure it's really what we're
+            // looking for.  We're assuming this is already an
+            // ACTION_STOCK_RESULT, else this would just be broken.
+            int reqId = intent.getIntExtra(StockService.EXTRA_REQUEST_ID, -1);
+            if(reqId != mWaitingOnThisOne) return;
+
+            // Well, it's what we're looking for.  What was the result?
+            int responseCode = intent.getIntExtra(StockService.EXTRA_RESPONSE_CODE, StockService.RESPONSE_NETWORK_ERROR);
+
+            switch(responseCode) {
+                case StockService.RESPONSE_OKAY:
+                    // Hey, would you look at that, it actually worked!  So, get
+                    // the Info out of it and fire it away!
+                    setInfo((Info)intent.getParcelableExtra(StockService.EXTRA_INFO));
+                    break;
+                case StockService.RESPONSE_NOT_POSTED_YET:
+                    // TODO: Error response
+                    mBanner.setText("NOT POSTED YET!");
+                    mBanner.setErrorStatus(ErrorBanner.Status.ERROR);
+                    mBanner.animateBanner(true);
+                    break;
+                case StockService.RESPONSE_NO_CONNECTION:
+                    // TODO: Also error response
+                    mBanner.setText("YOU CAN HAS NO CONNECTION YET!");
+                    mBanner.setErrorStatus(ErrorBanner.Status.ERROR);
+                    mBanner.animateBanner(true);
+                    break;
+                case StockService.RESPONSE_NETWORK_ERROR:
+                    // TODO: Again, error response
+                    mBanner.setText("RRRRRRRRRG STUPID NETWORK");
+                    mBanner.setErrorStatus(ErrorBanner.Status.ERROR);
+                    mBanner.animateBanner(true);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.centralmap);
+
+        mBanner = (ErrorBanner)findViewById(R.id.error_banner);
     }
 
     @Override
@@ -55,5 +114,10 @@ public class CentralMap extends Activity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void setInfo(Info info) {
+        // TODO: Something
+        mCurrentInfo = info;
     }
 }
---
+++ app/src/main/java/net/exclaimindustries/geohashdroid/widgets/ErrorBanner.java
@@ -0,0 +1,156 @@
+/*
+ * ErrorBanner.java
+ * Copyright (C) 2015 Nicholas Killewald
+ *
+ * This file is distributed under the terms of the BSD license.
+ * The source package should have a LICENSE file at the toplevel.
+ */
+
+package net.exclaimindustries.geohashdroid.widgets;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import net.exclaimindustries.geohashdroid.R;
+
+/**
+ * An <code>ErrorBanner</code> is meant to appear on the top of a View (usually
+ * the root View of an Activity) and slides in and out of place to report on
+ * errors, warnings, notifications, etc.
+ */
+public class ErrorBanner extends LinearLayout {
+    private TextView mMessage;
+
+    /**
+     * Use these in {@link #setErrorStatus(Status)} to set a premade
+     * background for these sorts of errors.
+     */
+    public enum Status {
+        /** A normal error banner (white). */
+        NORMAL,
+        /** An error banner screaming a warning (yellow). */
+        WARNING,
+        /** An error banner just giving up with an error (red). */
+        ERROR
+    }
+
+    public ErrorBanner(Context context) {
+        this(context, null);
+    }
+
+    public ErrorBanner(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        inflate(context, R.layout.error_banner, this);
+
+        // Get us the message widget for later.
+        mMessage = (TextView)findViewById(R.id.error_text);
+
+        // The button is always close.
+        findViewById(R.id.close).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Away it goes!
+                animateBanner(false);
+            }
+        });
+
+        // On startup, we want to make sure the view is off-screen until we're
+        // told different.  But, we can't just use setBannerVisible, as we
+        // haven't gone through layout yet, meaning we don't actually have a
+        // height.  So...
+        setTranslationY(-context.getResources().getDimension(R.dimen.error_banner_height));
+        setAlpha(0.0f);
+    }
+
+    /**
+     * Sets the banner's visibility.  Note that this is NOT the same as the View
+     * visibility (GONE, INVISIBLE, VISIBLE).  This sets whether or not the
+     * banner is hidden away off the top of the view and alpha'd to 0.  You
+     * would use this one to <i>instantly</i> change the visibility, not animate
+     * it.
+     *
+     * @param visible true to be visible, false to be hidden
+     */
+    public void setBannerVisible(boolean visible) {
+        if(!visible) {
+            // The translation should be negative the height of the view, which
+            // should neatly hide it away while allowing it to slide back in
+            // later if need be.
+            setTranslationY(-getHeight());
+            setAlpha(0.0f);
+        } else {
+            // Otherwise, it goes back to its normal spot.
+            setTranslationY(0.0f);
+            setAlpha(1.0f);
+        }
+    }
+
+    /**
+     * Slides the banner in or out of view.  Make sure it's set up first!
+     *
+     * @param visible true to show, false to slide away
+     */
+    public void animateBanner(boolean visible) {
+        if(!visible) {
+            // Slide out!
+            animate().translationY(-getHeight()).alpha(0.0f);
+        } else {
+            // Slide in!
+            animate().translationY(0.0f).alpha(1.0f);
+        }
+    }
+
+    /**
+     * Sets the text on the banner.
+     *
+     * @param text text to display
+     */
+    public void setText(final String text) {
+        ((Activity)getContext()).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mMessage.setText(text);
+            }
+        });
+    }
+
+    /**
+     * Convenience method to set a commonly-used background color.
+     *
+     * @param b type of background to set
+     */
+    public void setErrorStatus(Status b) {
+        switch(b) {
+            case NORMAL:
+                setBackgroundErrorColor(Color.WHITE);
+                break;
+            case WARNING:
+                setBackgroundErrorColor(Color.YELLOW);
+                break;
+            case ERROR:
+                setBackgroundErrorColor(Color.RED);
+                break;
+        }
+    }
+
+    /**
+     * Sets the background color.  For instance, you'd set this to red for
+     * errors, yellow for warnings, white if you forgot to change the color,
+     * that sort of thing.
+     *
+     * @param color the new color to set
+     */
+    public void setBackgroundErrorColor(final int color) {
+        ((Activity)getContext()).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                setBackgroundColor(color);
+            }
+        });
+    }
+}
