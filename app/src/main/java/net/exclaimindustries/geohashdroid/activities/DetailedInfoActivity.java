cd42beda734ebd226e1ad02c86521ea8c2af7b9c
Nicholas Killewald <captainspam@gmail.com>
2015-07-13 19:03:24 -0400

    Added menu options to DetailedInfoActivity.
    There!  Now it looks better.  Note that this time around, if a given
    external app isn't available (that is, anything that knows how to respond
    to the maps or radar intents), it won't show up in the menu at all.

--- app/src/main/java/net/exclaimindustries/geohashdroid/activities/DetailedInfoActivity.java
+++ app/src/main/java/net/exclaimindustries/geohashdroid/activities/DetailedInfoActivity.java
@@ -12,11 +12,17 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import net.exclaimindustries.geohashdroid.R;
 import net.exclaimindustries.geohashdroid.fragments.DetailedInfoFragment;
 import net.exclaimindustries.geohashdroid.util.Info;
+import net.exclaimindustries.tools.AndroidUtil;
+
+import java.text.DateFormat;
 
 /**
  * In the event this is a phone, <code>DetailedInfoActivity</code> holds the
@@ -30,26 +36,92 @@ public class DetailedInfoActivity extends Activity
      */
     public static final String INFO = "info";
 
+    private static final String SHOW_RADAR_ACTION = "com.google.android.radar.SHOW_RADAR";
+
+    private Info mInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.detail_activity);
 
-        // Slap in a fragment.  Make sure it isn't already there, of course.  It
-        // might be if we're coming back from, say, a config change.
+        // Grab the fragment.  We know it's there, it's right there in the
+        // layout.
         FragmentManager manager = getFragmentManager();
         DetailedInfoFragment frag = (DetailedInfoFragment) manager.findFragmentById(R.id.detail_fragment);
+
         // We'd BETTER have an Intent.
         Intent intent = getIntent();
 
         // And that intent BETTER have an Info.
-        Info inf = intent.getParcelableExtra(INFO);
+        mInfo = intent.getParcelableExtra(INFO);
 
         // Since the fragment's part of the layout, we can't set an argument
         // anymore.  So, just update the Info.
         frag.setCloseListener(this);
-        frag.setInfo(inf);
+        frag.setInfo(mInfo);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail_activity, menu);
+
+        // Now, some menu items may not be available if we can't get to them.
+        // Like, for instance, Send To Maps.  If Google Maps (or anything that
+        // can receive the Intent) isn't there, we can't do that.
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:0,0?q=loc:0,0"));
+        if(!AndroidUtil.isIntentAvailable(this, i))
+            menu.removeItem(R.id.action_send_to_maps);
+
+        // Or the Radar intent.
+        if(!AndroidUtil.isIntentAvailable(this, SHOW_RADAR_ACTION))
+            menu.removeItem(R.id.action_send_to_radar);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_preferences: {
+                // We've got preferences, so we've got an Activity.
+                Intent i = new Intent(this, PreferencesActivity.class);
+                startActivity(i);
+                return true;
+            }
+            case R.id.action_send_to_maps: {
+                // To the map!
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+
+                // Assemble the location.  This is a simple latitude,longitude
+                // setup.
+                String location = mInfo.getLatitude() + "," + mInfo.getLongitude();
+
+                // Then, toss the location out the door and hope whatever map
+                // we're using is paying attention.
+                i.setData(Uri.parse("geo:0,0?q=loc:"
+                        + location
+                        + "("
+                        + this.getString(
+                        R.string.send_to_maps_point_name,
+                        DateFormat.getDateInstance(DateFormat.LONG).format(
+                                mInfo.getCalendar().getTime())) + ")&z=15"));
+                startActivity(i);
+
+                return true;
+            }
+            case R.id.action_send_to_radar: {
+                Intent i = new Intent(SHOW_RADAR_ACTION);
+                i.putExtra("latitude", (float) mInfo.getLatitude());
+                i.putExtra("longitude", (float) mInfo.getLongitude());
+                startActivity(i);
+            }
+        }
+
+        return false;
     }
 
     @Override
