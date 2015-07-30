cdbda360c48baedd5146c06262fcf68d402d275f
Nicholas Killewald <captainspam@gmail.com>
2015-07-30 19:02:57 -0400

    Added createRatioPreservedDownscaledBitmapFromUri.
    No, I'm not going to rename those methods.  This one can read data from a
    URI directly (going through a ContentResolver).  This should patch up any
    troublesome issues from newer Gallery/Photos apps giving us images that
    don't really exist on the device.

--- app/src/main/java/net/exclaimindustries/tools/BitmapTools.java
+++ app/src/main/java/net/exclaimindustries/tools/BitmapTools.java
@@ -7,9 +7,15 @@
  */
 package net.exclaimindustries.tools;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.util.Log;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * BitmapTools are, as you probably guessed, tools for Bitmap manipulation.
@@ -147,6 +153,80 @@ public class BitmapTools {
         // The reversible flag is always false here, as we've already applied
         // it beforehand.
         return createRatioPreservedDownscaledBitmap(BitmapFactory.decodeFile(filename, opts), maxWidth, maxHeight, false);
+    }
+
+    /**
+     * Creates a new Bitmap that's a downscaled, ratio-preserved version of the
+     * content at a URI.  This will need to be something that ContentResolver
+     * can figure out, AND in a way that it can decode the image bounds properly
+     * before a full load.  If it can't do that, it'll return a null.
+     *
+     * @param context Context from which a ContentResolver can be retrieved
+     * @param uri URI of content to load
+     * @param maxWidth max width of new Bitmap, in pixels
+     * @param maxHeight max height of new Bitmap, in pixels
+     * @param reversible whether or not the ratio should be treated as
+     *                   reversible; that is, if the maxWidth and maxHeight are
+     *                   given as 800x600, but the image is 600x800, it will
+     *                   leave the image as 600x800 instead of reduce it to
+     *                   450x600
+     * @return a new, appropriately scaled Bitmap, or null if it failed entirely
+     * @throws IOException the URI could not be loaded for whatever reason
+     */
+    public static Bitmap createRatioPreservedDownscaledBitmapFromUri(Context context, Uri uri, int maxWidth, int maxHeight, boolean reversible)
+    throws IOException {
+        // Alright, ContentResolver.  You'd better do your job.
+        InputStream input = context.getContentResolver().openInputStream(uri);
+
+        // Otherwise, it's the same as before.  Grab the bounds only.
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(input, null, opts);
+        input.close();
+
+        // I'm reasonably certain that there's no oddities possible with the Uri
+        // in this case.  If the dimensions wind up -1, I'm just saying it can't
+        // open it, and if there's some obscure case where a legitimate Bitmap
+        // can be loaded whose dimensions are less than zero, I don't care.
+        if(opts.outHeight < 0 || opts.outWidth < 0) {
+            Log.e(DEBUG_TAG, "Error opening URI " + uri.toString());
+            return null;
+        }
+
+        // Do the same calculations as in the filename version...
+        if(reversible && shouldBeReversed(maxWidth, maxHeight, opts.outWidth, opts.outHeight)) {
+            int t = maxWidth;
+            maxWidth = maxHeight;
+            maxHeight = t;
+        }
+
+        int tempWidth = opts.outWidth;
+        int tempHeight = opts.outHeight;
+        int sampleFactor = 1;
+        while(true) {
+            if(tempWidth / 2 < maxWidth || tempHeight / 2 < maxHeight)
+                break;
+            tempWidth /= 2;
+            tempHeight /= 2;
+            sampleFactor *= 2;
+        }
+
+        Log.d(DEBUG_TAG, "Downsampling image to " + tempWidth + "x" + tempHeight + "...");
+
+        opts.inJustDecodeBounds = false;
+        opts.inSampleSize = sampleFactor;
+
+        // Re-open the stream, as we closed it already.
+        input = context.getContentResolver().openInputStream(uri);
+
+        // Read it into a Bitmap with the new options in hand.
+        Bitmap bitmap = BitmapFactory.decodeStream(input, null, opts);
+
+        // Close 'er up.
+        input.close();
+
+        // Let 'er rip.
+        return createRatioPreservedDownscaledBitmap(bitmap, maxWidth, maxHeight, false);
     }
 
     private static boolean shouldBeReversed(int inWidth, int inHeight, int outWidth, int outHeight) {
