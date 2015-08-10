eeb34022c81d68fe9c39ac78b0b5e579a6f8797f
Nicholas Killewald <captainspam@gmail.com>
2015-08-10 13:47:15 -0400

    Updated wiki destination.
    It's now at /geohashing/, not /wgh/.  This makes me wonder how long it's
    been that way, and makes me glad I'm finally fixing it now, however late it
    may be.

--- src/net/exclaimindustries/geohashdroid/WikiUtils.java
+++ src/net/exclaimindustries/geohashdroid/WikiUtils.java
@@ -48,7 +48,7 @@ import android.util.Log;
  */
 public class WikiUtils {
   /** The base URL for all wiki activities.  Remember the trailing slash! */
-  private static final String WIKI_BASE_URL = "http://wiki.xkcd.com/wgh/";
+  private static final String WIKI_BASE_URL = "http://wiki.xkcd.com/geohashing/";
   /** The URL for the MediaWiki API.  There's no trailing slash here. */
   private static final String WIKI_API_URL = WIKI_BASE_URL + "api.php";
 
